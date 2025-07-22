293aa7d0915297e5eeca9b7f62a681e93ad18fe8
Sameer Biswas <59316531+sameerkbiswas@users.noreply.github.com>
2025-07-22 11:58:15 +0530

    Create App.java

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins Maven World!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
